f1dd3a8584571fc1629b010f6ba5edae258c2b90
Erodri6z <erikrodriguez2417@gmail.com>
2024-09-08 19:26:59 -0500

    build out the drink dto

--- Cocktails.Api/Dtos/DrinkDto.cs
+++ Cocktails.Api/Dtos/DrinkDto.cs
@@ -1,6 +1,18 @@
 namespace Cocktails.Api.Dtos;
 
 public record class DrinkDto
-{
-  
-}
+(
+  int Id, 
+  string Name,
+  string MainSpirit,
+  string[] Ingredients,
+  decimal[] MeasurementsOz,
+  string[]? Garnish,
+  string Color,
+  string? Image,
+  string[] RecommendedGlasses,
+  string[] Notes,
+  string[] Method,
+  string? Credit,
+  string Vibe
+)
