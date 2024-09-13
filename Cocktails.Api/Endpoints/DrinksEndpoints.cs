19f0d595b5be6f937428256d76d03f1664453645
Erodri6z <erikrodriguez2417@gmail.com>
2024-09-13 00:19:55 -0500

    transfer all to endpoints

--- Cocktails.Api/Endpoints/DrinksEndpoints.cs
+++ Cocktails.Api/Endpoints/DrinksEndpoints.cs
@@ -56,4 +56,92 @@ public static class DrinksEndpoints
     "",
     "Chill"
   )];
+
+  public static WebApplication MapDrinksEndpoints(this WebApplication app)
+  {
+    
+    // Get all Drinks
+
+    app.MapGet("drinks", () => drinks);
+
+    // Get specific drinks
+    app.MapGet("drinks/{id}", (int id) => 
+    {
+      DrinkDto? drink = drinks.Find(drink => drink.Id == id);
+
+      return drink is null ? Results.NotFound() : Results.Ok(drinks);
+    })
+    .WithName(GetDrinkEndPointName);
+
+    // Get all Drinks based on main spirit
+
+    app.MapGet("drinks/ByAlcohol/{MainSpirit}", (string MainSpirit) => drinks.FindAll(drink => drink.MainSpirit == MainSpirit));
+
+    // Post a new drink
+
+    app.MapPost("drinks", (CreateDrinkDto newDrink) => {
+      DrinkDto drink = new(
+        drinks.Count + 1,
+        newDrink.Name,
+        newDrink.MainSpirit,
+        newDrink.Image,
+        newDrink.Ingredients,
+        newDrink.MeasurementsOz,
+        newDrink.Bitters,
+        newDrink.Garnish,
+        newDrink.Color,
+        newDrink.RecommendedGlasses,
+        newDrink.Notes,
+        newDrink.Method,
+        newDrink.Credit,
+        newDrink.Vibe
+      );
+      drinks.Add(drink);
+
+      return Results.CreatedAtRoute(GetDrinkEndPointName, new { id = drink.Id }, drink);
+    });
+
+    // Update a drink
+
+    app.MapPut("drinks/{id}", (int id, UpdateDrinkDto updatedDrink) =>
+    {
+      var index = drinks.FindIndex(drink => drink.Id == id);
+
+      if ( index == -1)
+      {
+        // TODO : if not able to find, create it with payload
+        return Results.NotFound();
+      }
+
+      drinks[index] = new DrinkDto(
+        id,
+        updatedDrink.Name,
+        updatedDrink.MainSpirit,
+        updatedDrink.Image,
+        updatedDrink.Ingredients,
+        updatedDrink.MeasurementsOz,
+        updatedDrink.Bitters,
+        updatedDrink.Garnish,
+        updatedDrink.Color,
+        updatedDrink.RecommendedGlasses,
+        updatedDrink.Notes,
+        updatedDrink.Method,
+        updatedDrink.Credit,
+        updatedDrink.Vibe
+      );
+
+      return Results.NoContent();
+    });
+
+    // Delete a drink
+
+    app.MapDelete("drinks/{id}", (int id) => 
+    {
+      drinks.RemoveAll(drink => drink.Id == id);
+
+      return Results.NoContent();
+    });
+    
+    return app;
+  }
 }
