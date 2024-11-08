54a1fcbddaabf71e8e64e8c946a2968e9bf8d69e
Erodri6z <erikrodriguez2417@gmail.com>
2024-11-07 21:18:13 -0600

    load .env

--- Cocktails.Api/Program.cs
+++ Cocktails.Api/Program.cs
@@ -1,7 +1,11 @@
 using Cocktails.Api.Dtos;
 using Cocktails.Api.Endpoints;
+using DotNetEnv;
 
 var builder = WebApplication.CreateBuilder(args);
+
+Env.Load();
+
 var app = builder.Build();
 
 app.MapDrinksEndpoints();
