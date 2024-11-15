30dbd186cbdb5add8ca5e48c831fc452ce73b58e
Erodri6z <erikrodriguez2417@gmail.com>
2024-11-14 18:43:08 -0600

    stub up the validation attribute

--- Cocktails.Api/Attributes/NonEmptyArrayAttribute.cs
+++ Cocktails.Api/Attributes/NonEmptyArrayAttribute.cs
@@ -1,8 +1,20 @@
 using System;
+using System.ComponentModel.DataAnnotations;
 
 namespace Cocktails.Api.Attributes;
 
-public class NonEmptyArrayAttribute
-{
+  [AttributeUsage(AttributeTargets.Property, AllowMultiple = false)]
+  public class NonEmptyArrayAttribute : ValidationAttribute
+  {
+    protected override ValidationResult? IsValid(object? value, ValidationContext validationContext)
+    {
+      if (value is Array array && array.Length == 0)
+      {
+        return new ValidationResult($"The {validationContext.DisplayName} field cannot be an empty array.");
+      }
+      return ValidationResult.Success;
+    }
+  }
+  
 
-}
+
