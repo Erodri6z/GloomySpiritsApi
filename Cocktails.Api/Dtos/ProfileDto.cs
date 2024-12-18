ab70c44d655a5b8f0c2cb9a36235fcd94198425a
Erodri6z <erikrodriguez2417@gmail.com>
2024-12-17 20:04:33 -0600

    edit the profile dto

--- Cocktails.Api/Dtos/ProfileDto.cs
+++ Cocktails.Api/Dtos/ProfileDto.cs
@@ -3,26 +3,13 @@ using MongoDB.Bson.Serialization.Attributes;
 
 namespace Cocktails.Api.Dtos
 {
-    public class ProfileDto
-    {
-        [BsonId]
-        [BsonRepresentation(BsonType.ObjectId)]
-        public string Id { get; set; }
-
-        [BsonElement("username")]
-        public string Username { get; set; } = string.Empty;
-
-        [BsonElement("email")]
-        public string Email { get; set; } = string.Empty;
-
-        [BsonElement("profileImageUrl")]
-        public string? ProfileImageUrl { get; set; }
-
-        [BsonElement("createdAt")]
-        [BsonDateTimeOptions(Kind = DateTimeKind.Utc)]
-        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
-
-        [BsonElement("updatedAt")]
-        [BsonDateTimeOptions(Kind = DateTimeKind.Utc)]
-        public DateTime UpdatedAt { get; set; } = DateTime.UtcNow;
+  public class ProfileDto
+  {
+  [BsonId]
+  [BsonRepresentation(BsonType.ObjectId)]
+  public string Id { get; set; }
+  [Required] public string Username { get; set; }
+  [Required] public string Email { get; set; } = string.Empty;
+  [Required]  DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+  [Required] public DateTime UpdatedAt { get; set; } = DateTime.UtcNow;
 }
