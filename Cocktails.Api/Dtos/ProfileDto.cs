f754fee21fc2fe7cb0465154abade3902d6fa9ce
Erodri6z <erikrodriguez2417@gmail.com>
2024-12-17 21:40:43 -0600

    stub up a profile  endpoints file

--- Cocktails.Api/Dtos/ProfileDto.cs
+++ Cocktails.Api/Dtos/ProfileDto.cs
@@ -8,9 +8,20 @@ public record class ProfileDto
     [BsonId]
     [BsonRepresentation(BsonType.ObjectId)]
     public string Id { get; set; }
-    [Required] public string Username { get; set; }
-    [Required] public string Email { get; set; } = string.Empty;
-    [Required]  public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
-    [Required] public DateTime UpdatedAt { get; set; } = DateTime.UtcNow;
+
+    [BsonElement("Username")]
+    public string Username { get; set; }
+
+    [BsonElement("Email")]
+    public string Email { get; set; }
+
+    [BsonElement("PasswordHash")]
+    public string PasswordHash { get; set; }
+
+    [BsonElement("FullName")]
+    public string FullName { get; set; }
+
+    [BsonElement("DateOfBirth")]
+    public DateTime DateOfBirth { get; set; }
   }
 }
---
+++ Cocktails.Api/Endpoints/ProfileEndpoints.cs
@@ -0,0 +1,10 @@
+using Cocktails.Api.Data;
+using Cocktails.Api.Dtos;
+
+
+namespace Cocktails.Api.Endpoints; 
+
+public static class ProfileEndpoints
+{
+  const string ProfileEndpointName = "GetProfile";
+}
