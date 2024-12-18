0bb8420fdfe0e50ad63b2c0b30912ed42d3d5592
Erodri6z <erikrodriguez2417@gmail.com>
2024-12-17 20:28:14 -0600

    fix a missing curly brace error

--- Cocktails.Api/Dtos/ProfileDto.cs
+++ Cocktails.Api/Dtos/ProfileDto.cs
@@ -1,15 +1,16 @@
 using MongoDB.Bson;
 using MongoDB.Bson.Serialization.Attributes;
-
+      
 namespace Cocktails.Api.Dtos
 {
-  public class ProfileDto
+public record class ProfileDto
   {
-  [BsonId]
-  [BsonRepresentation(BsonType.ObjectId)]
-  public string Id { get; set; }
-  [Required] public string Username { get; set; }
-  [Required] public string Email { get; set; } = string.Empty;
-  [Required]  DateTime CreatedAt { get; set; } = DateTime.UtcNow;
-  [Required] public DateTime UpdatedAt { get; set; } = DateTime.UtcNow;
+    [BsonId]
+    [BsonRepresentation(BsonType.ObjectId)]
+    public string Id { get; set; }
+    [Required] public string Username { get; set; }
+    [Required] public string Email { get; set; } = string.Empty;
+    [Required]  public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+    [Required] public DateTime UpdatedAt { get; set; } = DateTime.UtcNow;
+  }
 }
